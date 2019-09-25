20038aadf82540624be8052b06f6a9580356d12b
Xyrille Ann Mamalateo <xyrilleannm@gmail.com>
2019-09-25 16:21:22 +0800

    XyrilleMamalateo.Midterm2

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XyrilleAnnMamalateo.Midterm2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int v1 = int.Parse(txtFrom.Text);
+            int v2 = int.Parse(txtTo.Text);
+            int v3;
+            int v4 = v1;
+            int v5 = v1;
+            for (v3 = v2 - v1; v3 != 0; --v3)
+            {
+                ++v4;
+                v5 = v4 + v5;
+            }
+            lblResult.Content = v5.ToString();
+        }
+    }
+}
